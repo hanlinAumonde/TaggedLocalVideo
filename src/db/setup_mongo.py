1a9f93ad2b0d85296671f96c5d88a5a10d8cdf41
wuhanlin <841116184@qq.com>
2026-01-08 11:33:16 +0100

    Fix MongoDB URI construction and ensure correct database initialization

--- src/db/setup_mongo.py
+++ src/db/setup_mongo.py
@@ -8,11 +8,13 @@ async def setup_mongo():
     mongo_config: MongoConfig = get_settings().mongo
     #create an async MongoDB client
     mongo_uri = "mongodb://"
+    mongo_host_port = f"{mongo_config.host}:{mongo_config.port}"
     if mongo_config.username and mongo_config.password:
-        mongo_uri += f"{mongo_config.username}:{mongo_config.password}@"
-    mogno_uri += f"{mongo_config.host}:{mongo_config.port}"
+        mongo_uri += f"{mongo_config.username}:{mongo_config.password}@" + mongo_host_port + f"/{mongo_config.database}?authSource={mongo_config.database}"
+    else:
+        mongo_uri += mongo_host_port
     client = AsyncIOMotorClient(mongo_uri)
 
     #initialize Beanie with the client and database name
-    await init_beanie(database=client.video_tag_db, document_models=[VideoModel, VideoTagModel])
+    await init_beanie(database=client.get_database(mongo_config.database), document_models=[VideoModel, VideoTagModel])
     print("MongoDB setup complete")
